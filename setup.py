ccc2e660515bbe33d67ef6b7309979dab691ca94
Niels Justesen <njustesen@gmail.com>
2018-12-27 21:17:37 +0100

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bb',
+      version="0.0.1",
+      install_requires=[
+          'numpy',
+          'untangle',
+          'Flask',
+          'gym',
+          'Jinja2',
+          'untangle'
+      ])
