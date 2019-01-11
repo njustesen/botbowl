3de36f99009ba9546c1fbc1cf2ba009d91362ed4
Christy42 <mark.christiansen@nuim.ie>
2019-01-11 22:42:31 +0000

    numpy....randint(1,6) is half open.  This ensures that a maximum can be rolled

--- ffai/core/model.py
+++ ffai/core/model.py
@@ -718,7 +718,7 @@ class DiceRoll:
 class D3(Die):
 
     def __init__(self, rnd):
-        self.value = rnd.randint(1, 3)
+        self.value = rnd.randint(1, 4)
 
     def get_value(self):
         return self.value
@@ -733,7 +733,7 @@ class D3(Die):
 class D6(Die):
 
     def __init__(self, rnd):
-        self.value = rnd.randint(1, 6)
+        self.value = rnd.randint(1, 7)
 
     def get_value(self):
         return self.value
@@ -748,7 +748,7 @@ class D6(Die):
 class D8(Die):
 
     def __init__(self, rnd):
-        self.value = rnd.randint(1, 8)
+        self.value = rnd.randint(1, 9)
 
     def get_value(self):
         return self.value
@@ -763,7 +763,7 @@ class D8(Die):
 class BBDie(Die):
 
     def __init__(self, rnd):
-        r = rnd.randint(1, 6)
+        r = rnd.randint(1, 7)
         if r == 6:
             r = 3
         self.value = BBDieResult(r)
