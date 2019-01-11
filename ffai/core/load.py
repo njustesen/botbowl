c9a1baeaf6a3288c1f535eb56dae9473fa5e48be
Christy42 <mark.christiansen@nuim.ie>
2019-01-11 00:09:34 +0000

    Use os functionality for the file path manipulation

--- ffai/core/load.py
+++ ffai/core/load.py
@@ -156,7 +156,7 @@ def get_team_by_id(team_id, ruleset):
     """
     path = get_data_path('teams/')
     for file in list(glob.glob(path + '/*.json')):
-        name = file.split("/")[-1].split("\\")[-1].split(".")[0]
+        name = os.path.split(file)[1].split(".")[0]
         team = get_team(name, ruleset)
         if team.team_id == team_id:
             return team
