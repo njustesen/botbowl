698e9f36cd644670ec8c13dae69cfe9616632617
Niels Justesen <njustesen@gmail.com>
2019-03-06 13:06:30 +0100

    end setup action removed from gym env

--- ffai/ai/env.py
+++ ffai/ai/env.py
@@ -40,7 +40,6 @@ class FFAIEnv(gym.Env):
         ActionType.TAILS,
         ActionType.KICK,
         ActionType.RECEIVE,
-        ActionType.END_SETUP,
         ActionType.END_PLAYER_TURN,
         ActionType.USE_REROLL,
         ActionType.DONT_USE_REROLL,
