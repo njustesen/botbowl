dd82cae62ac7aeb4aa2b60199c8ce11f1ff58c2a
Dan Johnson <danj3000@outlook.com>
2019-11-08 14:59:18 +0000

    add tests for thick skull

--- tests/test_injury.py
+++ tests/test_injury.py
@@ -121,27 +121,75 @@ def test_injury_stunned_with_stunty(mock_game):
         proc = stack.peek()
         assert isinstance(proc, KnockOut)
 
+
 @patch("ffai.core.game.Game")
 def test_injury_ko_with_stunty(mock_game):
     # patch the mock game proc stack
     stack = Stack()
-    mock_game.state.stack = stack    
+    mock_game.state.stack = stack
     with patch("ffai.core.util.Stack", new_callable=PropertyMock) as a:
-        a.return_value=stack
+        a.return_value = stack
 
         # fix the dice rolls - 3+6 = 9 => CAS for stunty
         D6.FixedRolls.clear()
         D6.FixedRolls.append(3)
         D6.FixedRolls.append(6)
 
-        role = Role("Blitzer", "orc", 6,3,3,9, [], 50000, None)
+        role = Role("Blitzer", "orc", 6, 3, 3, 9, [], 50000, None)
         player = Player("1", role, "test", 1, "orc", extra_skills=[Skill.STUNTY])
         blocker = Player("1", role, "test", 1, "orc")
         injury = Injury(mock_game, player, inflictor=blocker)
         injury.step(action=None)
-        
+
         # stunty should make it a Casualty
         proc = stack.peek()
         assert isinstance(proc, Casualty)
 
+
+@patch("ffai.core.game.Game")
+def test_injury_ko_with_thick_skull(mock_game):
+    # patch the mock game proc stack
+    stack = Stack()
+    mock_game.state.stack = stack
+    with patch("ffai.core.util.Stack", new_callable=PropertyMock) as a:
+        a.return_value = stack
+
+        # fix the dice rolls - 4+4 = 8 => stunned for Thick skull
+        D6.FixedRolls.clear()
+        D6.FixedRolls.append(4)
+        D6.FixedRolls.append(4)
+
+        role = Role("Blitzer", "orc", 6, 3, 3, 9, [], 50000, None)
+        player = Player("1", role, "test", 1, "orc", extra_skills=[Skill.THICK_SKULL])
+        blocker = Player("1", role, "test", 1, "orc")
+        injury = Injury(mock_game, player, inflictor=blocker)
+        injury.step(action=None)
+
+        # thick skull should make it Stunned => Injury
+        proc = stack.peek()
+        assert isinstance(proc, Injury)
+
+
+@patch("ffai.core.game.Game")
+def test_injury_cas_with_thick_skull(mock_game):
+    # patch the mock game proc stack
+    stack = Stack()
+    mock_game.state.stack = stack
+    with patch("ffai.core.util.Stack", new_callable=PropertyMock) as a:
+        a.return_value = stack
+
+        # fix the dice rolls - 5+5 = 10 => no effect for Thick skull
+        D6.FixedRolls.clear()
+        D6.FixedRolls.append(5)
+        D6.FixedRolls.append(5)
+
+        role = Role("Blitzer", "orc", 6, 3, 3, 9, [], 50000, None)
+        player = Player("1", role, "test", 1, "orc", extra_skills=[Skill.THICK_SKULL])
+        blocker = Player("1", role, "test", 1, "orc")
+        injury = Injury(mock_game, player, inflictor=blocker)
+        injury.step(action=None)
+
+        # thick skull has no effect - it's a casualty
+        proc = stack.peek()
+        assert isinstance(proc, Casualty)
 
