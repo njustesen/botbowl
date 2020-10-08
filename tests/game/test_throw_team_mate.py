db7febdef3a84f06bbb01c2589c5115a75d0baf1
mrbermell <31434035+mrbermell@users.noreply.github.com>
2020-10-08 22:12:18 +0200

    Create test_throw_team_mate.py

---
+++ tests/game/test_throw_team_mate.py
@@ -0,0 +1,113 @@
+from tests.util import *
+import pytest
+
+def test_throw_TM_success():
+    game = get_game_turn()
+    team = game.get_agent_team(game.actor)
+    game.clear_board()
+    game.state.weather = WeatherType.NICE
+    game.state.teams[0].state.rerolls = 0
+    
+    thrower = team.players[0]
+    thrower.extra_skills = [Skill.THROW_TEAM_MATE]
+    game.put(thrower, Square(1, 1))
+    
+    thrown_player = team.players[1]
+    thrown_player.extra_skills = [Skill.RIGHT_STUFF]
+    game.put(thrown_player, Square(1, 2))
+    game.get_ball().move_to(thrown_player.position)
+    game.get_ball().is_carried = True
+    
+    game.set_available_actions() 
+    game.state.reports.clear() 
+    
+    D6.fix_result(6) # throw 
+    
+    D8.fix_result(5) # scatter
+    D8.fix_result(5)
+    D8.fix_result(5)
+    
+    D6.fix_result(6) # landing 
+    
+    game.step(Action(ActionType.START_PASS, player=thrower))
+    assert game.has_report_of_type(OutcomeType.PASS_ACTION_STARTED)
+    assert ActionType.SELECT_PLAYER in [a.action_type for a in game.get_available_actions()] 
+    
+    game.step(Action(ActionType.SELECT_PLAYER, position=thrown_player.position ))
+    assert not game.is_pass_available()
+    
+    game.step(Action(ActionType.PASS, position=Square(4,4) ))
+    assert thrown_player.state.up 
+    assert not thrown_player.state.used
+    assert thrown_player.position == Square(7,4)
+    assert game.get_ball().position == Square(7,4)
+    
+    assert game.has_report_of_type(OutcomeType.PASS_ACTION_STARTED)
+    assert game.has_report_of_type(OutcomeType.TEAM_MATE_THROWN)
+    assert game.has_report_of_type(OutcomeType.THROWN_TEAM_MATE_SCATTER)
+    assert game.has_report_of_type(OutcomeType.RIGHT_STUFF_LANDING_SUCCESS)
+    
+    
+@pytest.mark.parametrize("break_armor", [True, False]) 
+def test_throw_TM_failed_landing(break_armor):
+    game = get_game_turn()
+    team = game.get_agent_team(game.actor)
+    game.clear_board()
+    game.state.weather = WeatherType.NICE
+    game.state.teams[0].state.rerolls = 0
+    
+    thrower = team.players[0]
+    thrower.extra_skills = [Skill.THROW_TEAM_MATE]
+    game.put(thrower, Square(1, 1))
+    
+    thrown_player = team.players[1]
+    thrown_player.extra_skills = [Skill.RIGHT_STUFF]
+    game.put(thrown_player, Square(1, 2))
+    game.get_ball().move_to(thrown_player.position)
+    game.get_ball().is_carried = True
+
+    game.set_available_actions() 
+    game.state.reports.clear() 
+    
+    D6.fix_result(6) # throw 
+    
+    D8.fix_result(5) # scatter 
+    D8.fix_result(5)
+    D8.fix_result(5)
+    
+    D6.fix_result(1) # landing 
+    
+    D8.fix_result(5) #ball bounce  
+    
+    if break_armor: 
+        D6.fix_result(6) # armor 1
+        D6.fix_result(6) # armor 2 
+        D6.fix_result(1) # injury 1
+        D6.fix_result(1) # injury 2 
+    else: 
+        D6.fix_result(1) # armor 1
+        D6.fix_result(1) # armor 2 
+        
+    game.step(Action(ActionType.START_PASS, player=thrower))
+    assert game.has_report_of_type(OutcomeType.PASS_ACTION_STARTED)
+    assert ActionType.SELECT_PLAYER in [a.action_type for a in game.get_available_actions()] 
+    
+    game.step(Action(ActionType.SELECT_PLAYER, position=thrown_player.position ))
+    assert not game.is_pass_available()
+    
+    game.step(Action(ActionType.PASS, position=Square(4,4) ))
+    
+    assert thrown_player.position == Square(7,4)
+    assert game.get_ball().position == Square(8,4)
+    assert not thrown_player.state.up 
+    if break_armor: 
+        assert thrown_player.state.stunned
+    else: 
+        assert not thrown_player.state.used  
+    
+    assert game.has_report_of_type(OutcomeType.PASS_ACTION_STARTED)
+    assert game.has_report_of_type(OutcomeType.TEAM_MATE_THROWN)
+    assert game.has_report_of_type(OutcomeType.THROWN_TEAM_MATE_SCATTER)
+    assert game.has_report_of_type(OutcomeType.RIGHT_STUFF_LANDING_FAILURE)
+    
+    
