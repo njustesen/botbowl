08e0c2af5ac3cce11f65f5f3e38208391321a41e
mrbermell <mattias.br@gmail.com>
2021-03-19 14:31:50 +0100

    skeleton of test_vector_env.py

---
+++ examples/a3c/tests/test_vector_env.py
@@ -0,0 +1,21 @@
+import gym
+import torch
+from ffai.ai.bots.random_bot import *
+
+
+def test_worker_memory():
+    pass
+
+
+def test_vector_memory():
+    pass
+
+
+def test_simple_vector_environment():
+    pass
+
+
+def test_multiprocess_vector_environment():
+    pass
+
+
