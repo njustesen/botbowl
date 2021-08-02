456a5f77fd0d6a0c4863fdfed393c88f85704db1
Niels Justesen <njustesen@gmail.com>
2021-08-02 12:00:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ffai',
-      version="0.3.0",
+      version="0.3.1",
       include_package_data=True,
       install_requires=[
           'numpy',
