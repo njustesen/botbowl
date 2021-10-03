8130e22c6ea893a1a6faaae183d3fed21f231964
mrbermell <mattias.br@gmail.com>
2021-10-03 22:39:07 +0200

    Compile only when cython is installed

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
-from Cython.Build import cythonize
 
-import Cython.Compiler.Options
-Cython.Compiler.Options.annotate = True
+try:
+    from Cython.Build import cythonize
+    import Cython.Compiler.Options
+    Cython.Compiler.Options.annotate = True
+    cython_exists = True
+except ImportError:
+    cython_exists = False
 
-setup(name='ffai',
-      version="0.3.2",
-      include_package_data=True,
-      install_requires=[
+files_to_compile = ["ffai/ai/fast_pathing.pyx"]
+
+install_requires_packages = [
           'numpy',
           'untangle',
           'Flask',
@@ -18,9 +21,19 @@ setup(name='ffai',
           'requests',
           'pytest',
           'matplotlib'
-      ],
-      packages=find_packages(),
-      ext_modules=cythonize(["ffai/ai/fast_pathing.pyx"], annotate=True),
-      zip_safe=False
-)
+]
+
+kwargs = {
+    'name': 'ffai',
+    'version': '0.3.2',
+    'include_package_data': True,
+    'install_requires': install_requires_packages,
+    'packages': find_packages(),
+    'zip_safe': False
+}
+
+if cython_exists:
+    kwargs['ext_modules'] = cythonize(files_to_compile, annotate=True)
+
+setup(**kwargs)
 
