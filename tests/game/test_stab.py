8ccf300226c5a4c6666f1df2a92144664f93e3af
Niels Justesen <njustesen@gmail.com>
2021-10-08 12:11:43 +0200

    failing blitz test added

--- tests/game/test_stab.py
+++ tests/game/test_stab.py
@@ -21,11 +21,16 @@ def test_stab_success_kill():
 
 def test_stab_blitz():
     game = get_game_turn()
+    game.config.pathfinding_enabled = True
     team = game.get_agent_team(game.actor)
-    team.state.rerolls = 0
-    attacker, defender = get_block_players(game, team)
-    attacker.role.skills = [Skill.STAB]
-    game.step(Action(ActionType.START_BLITZ, player=attacker))
+    other_team = game.get_opp_team(team)
+    game.clear_board()
+    stabber = team.players[0]
+    stabber.role.skills = [Skill.STAB]
+    defender = other_team.players[0]
+    game.put(stabber, Square(1, 1))
+    game.put(defender, Square(3, 3))
+    game.step(Action(ActionType.START_BLITZ, player=stabber))
     D6.fix(6)
     D6.fix(6)
     D6.fix(6)
