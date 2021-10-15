70b6c979fb67f6286783f94133cff181c7191410
mrbermell <mattias.br@gmail.com>
2021-10-15 11:46:49 +0200

    benchmark for cython pathfinding

---
+++ tests/performance/benchmark_cython_pathfinding.py
@@ -0,0 +1,26 @@
+import timeit
+from tests.util import get_game_turn
+import ffai.ai.pathfinding as python_pathfinding
+import ffai.ai.fast_pathing as cython_pathfinding
+
+def benchmark_cython_pathfinding():
+    game = get_game_turn(empty=False)
+    players_on_pitch = game.get_players_on_pitch()
+
+    def get_paths_of_all_players(pf):
+        for player in players_on_pitch:
+            pf.Pathfinder(game, player, trr=True).get_paths()
+
+
+    scope = {'get_paths_of_all_players': get_paths_of_all_players,
+             'cython_pathfinding': cython_pathfinding,
+             'python_pathfinding': python_pathfinding}
+
+    cython_time = timeit.timeit("get_paths_of_all_players(cython_pathfinding)",number=2, globals=scope)
+    python_time = timeit.timeit("get_paths_of_all_players(python_pathfinding)",number=2, globals=scope)
+
+    print(f"{cython_time=}")
+    print(f"{python_time=}")
+
+if __name__ == "__main__":
+    benchmark_cython_pathfinding()
