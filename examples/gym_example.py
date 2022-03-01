6139c92b5a499d2037a08705525a0fc55f5ab0cb
mrbermell <mattias.br@gmail.com>
2022-03-01 09:57:41 +0100

    update according to review comments

--- examples/gym_example.py
+++ examples/gym_example.py
@@ -12,13 +12,13 @@ def main():
     # Play 10 games
     for _ in range(10):
         done = False
-        _, _, mask = env.reset()
+        spatial_obs, non_spatial_obs, mask = env.reset()
 
         while not done:
             env.render(feature_layers=True)
             aa = np.where(mask > 0.0)[0]
             action_idx = np.random.choice(aa, 1)[0]
-            (_, _, mask), _, done, _ = env.step(action_idx)
+            (spatial_obs, non_spatial_obs, mask), reward, done, info = env.step(action_idx)
             steps += 1
             print(steps)
 
