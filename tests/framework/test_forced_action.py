605faff2a68294dd56833fa9726070737e3744dd
Niels Justesen <njustesen@gmail.com>
2022-03-10 12:07:42 +0100

    test entire game with timeoutes + fixes

--- tests/framework/test_forced_action.py
+++ tests/framework/test_forced_action.py
@@ -3,6 +3,26 @@ from botbowl.core.load import *
 from botbowl.core.game import Game
 
 
+def test_forced_action():
+    config = load_config("gym-11")
+    config.time_limits.turn = 0.01
+    config.time_limits.secondary = 0.01
+    config.competition_mode = True
+    config.fast_mode = True
+    ruleset = load_rule_set(config.ruleset)
+    home = load_team_by_filename("human", ruleset)
+    away = load_team_by_filename("human", ruleset)
+    home_agent = Agent("human1", human=True)
+    away_agent = Agent("human2", human=True)
+    game = Game(1, home, away, home_agent, away_agent, config)
+    game.init()
+    game.step(Action(ActionType.START_GAME))
+    for i in range(100):
+        time.sleep(config.time_limits.turn * 1.5)
+        game.refresh()
+    assert game.state.game_over
+
+
 def test_forced_setup():
     config = load_config("gym-11")
     config.time_limits.turn = 0.1
